0cd3a88a754046456ceea3e425bf2df187c1457e
Cary Snyder <csnyder1978@gmail.com>
2025-01-30 23:05:10 -0500

    update to remove field

--- dice_roll.py
+++ dice_roll.py
@@ -4,9 +4,8 @@ def roll(n):
    dx = int(int(input("How many sided dice?" )))
    for i in range(n):
       print(np.random.randint(1, [dx]))
-def greet():
-      tries = 1
-      dice = int(input("Hello friends, how many dice? "))
-      for j in range(tries):
+def greet():    
+     dice = int(input("Hello friends, how many dice? "))
+     for j in range(1):
         roll(dice)
 greet()
